078a3c6d799301d6dccfe19e203d77b9d0830cd5
gdprasad1201 <gdprasad@crimson.ua.edu>
2024-09-13 09:38:35 -0500

    Delete Fall2024-Assignment1-gdprasad directory

--- Fall2024-Assignment1-gdprasad/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fall2024_Assignment1_gdprasad.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
